9cc52c7f3b1a40bf4c8a5aed2251127d5ac44619
atomdeniz <38922410+atomdeniz@users.noreply.github.com>
2021-12-17 18:51:40 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,7 +1,6 @@
 'use strict';
 const express = require('express');
 const path = require('path');
-const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -18,4 +17,3 @@ app.use(bodyParser.json());
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
-module.exports.handler = serverless(app);
